307b6f6a55750f9505c1554ec57eae0355835946
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-07-29 15:39:01 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__, 
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Static file routes (should work automatically, but explicit is better)
 @app.route('/static/<path:path>')
-def serve_static(path):
+def static_files(path):
     return send_from_directory('static', path)
+
+@app.route('/backgrounds/<path:filename>')
+def backgrounds(filename):
+    return send_from_directory(os.path.join('static', 'backgrounds'), filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
