b26b24ba5c99340f75ae5da7d30c50351dde7fa8
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-08-02 11:12:50 -0400

    Add Google Analytics 4 tracking and event monitoring

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google AdSense */}
+        {/* Google Analytics */}
+        <script 
+          async 
+          src="https://www.googletagmanager.com/gtag/js?id=G-QMD6NEPFWR"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QMD6NEPFWR', {
+                page_title: document.title,
+                page_location: window.location.href,
+              });
+            `,
+          }}
+        />
+        
+        {/* Google AdSense (your existing code) */}
         <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2391004325385163"
