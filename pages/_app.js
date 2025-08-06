18f6549be556492566b919bed4ca49ad1dec855a
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-08-06 11:20:57 -0400

    Fix image loading and performance optimizations - use cleaned metadata with 93 valid images

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,5 @@
 import '../styles/globals.css'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-  
-  useEffect(() => {
-    // Only run on client side
-    if (typeof window !== 'undefined') {
-      const handleRouteChange = (url) => {
-        gtag.pageview(url)
-      }
-      
-      // Track page views
-      router.events.on('routeChangeComplete', handleRouteChange)
-      
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange)
-      }
-    }
-  }, [router.events])
-
   return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-function MyDocument() {
+export default function Document() {
   return (
     <Html lang="en">
       <Head>
@@ -9,32 +9,6 @@ function MyDocument() {
         <link rel="preconnect" href="https://pagead2.googlesyndication.com" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
-
-        {/* Google Analytics - deferred */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-QMD6NEPFWR"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-QMD6NEPFWR', {
-                page_title: document.title,
-                page_location: window.location.href,
-              });
-            `,
-          }}
-        />
-        
-        {/* Google AdSense - deferred */}
-        <script 
-          defer
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2391004325385163"
-          crossOrigin="anonymous"
-        />
         
         {/* Favicon and meta */}
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
@@ -44,26 +18,8 @@ function MyDocument() {
       </Head>
       <body>
         <Main />
-        <DeferredNextScript />
+        <NextScript />
       </body>
     </Html>
   )
 }
-
-// Custom NextScript component that uses defer instead of async
-function DeferredNextScript() {
-  return (
-    <>
-      {process.env.NODE_ENV === 'development' ? (
-        <NextScript />
-      ) : (
-        <script
-          defer
-          src="/_next/static/chunks/webpack.js"
-        />
-      )}
-    </>
-  )
-}
-
-export default MyDocument
