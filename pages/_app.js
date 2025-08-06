87fdf5fb81a7e8c3b48edc8822db4c29fbc20d71
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-08-06 11:14:57 -0400

    Fix image loading and performance optimizations - use cleaned metadata with 93 valid images

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,18 @@ export default function App({ Component, pageProps }) {
   const router = useRouter()
   
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    
-    // Track page views
-    router.events.on('routeChangeComplete', handleRouteChange)
-    
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
+    // Only run on client side
+    if (typeof window !== 'undefined') {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      
+      // Track page views
+      router.events.on('routeChangeComplete', handleRouteChange)
+      
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
     }
   }, [router.events])
 
