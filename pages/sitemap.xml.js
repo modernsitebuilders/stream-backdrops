9f4a4ffc0dab659629cb74bcb59a9467f645f481
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-08-11 08:36:49 -0400

    sitemap update

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -24,6 +24,24 @@ function generateSiteMap() {
        <priority>0.8</priority>
      </url>
      <url>
+       <loc>https://streambackdrops.com/blog</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>weekly</changefreq>
+       <priority>0.8</priority>
+     </url>
+     <url>
+       <loc>https://streambackdrops.com/blog-virtual-background-guide</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>monthly</changefreq>
+       <priority>0.7</priority>
+     </url>
+     <url>
+       <loc>https://streambackdrops.com/license</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>monthly</changefreq>
+       <priority>0.7</priority>
+     </url>
+     <url>
        <loc>https://streambackdrops.com/premium</loc>
        <lastmod>${new Date().toISOString()}</lastmod>
        <changefreq>weekly</changefreq>
@@ -44,31 +62,19 @@ function generateSiteMap() {
      
      <!-- Category Pages -->
      <url>
-       <loc>https://streambackdrops.com/category/home-lifestyle</loc>
+       <loc>https://streambackdrops.com/category/well-lit</loc>
        <lastmod>${new Date().toISOString()}</lastmod>
        <changefreq>weekly</changefreq>
        <priority>0.9</priority>
      </url>
      <url>
-       <loc>https://streambackdrops.com/category/home-lifestyle</loc>
+       <loc>https://streambackdrops.com/category/ambiant</loc>
        <lastmod>${new Date().toISOString()}</lastmod>
        <changefreq>weekly</changefreq>
        <priority>0.9</priority>
      </url>
      <url>
-       <loc>https://streambackdrops.com/category/professional-shelves</loc>
-       <lastmod>${new Date().toISOString()}</lastmod>
-       <changefreq>weekly</changefreq>
-       <priority>0.9</priority>
-     </url>
-     <url>
-       <loc>https://streambackdrops.com/category/professional-shelves</loc>
-       <lastmod>${new Date().toISOString()}</lastmod>
-       <changefreq>weekly</changefreq>
-       <priority>0.9</priority>
-     </url>
-     <url>
-       <loc>https://streambackdrops.com/category/professional-shelves</loc>
+       <loc>https://streambackdrops.com/category/office-spaces</loc>
        <lastmod>${new Date().toISOString()}</lastmod>
        <changefreq>weekly</changefreq>
        <priority>0.9</priority>
