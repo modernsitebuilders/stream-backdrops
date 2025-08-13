00ed84ce88d7fc86d08fed60ff3dda3c9a5140ce
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-08-13 13:07:16 -0400

    increasing SEO in pages/document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,47 @@ export default function Document() {
   return (
     <Html lang="en" data-scroll-behavior="smooth">
       <Head>
-        {/* Preconnect to external domains */}
+        {/* Preconnect to external domains for faster loading */}
         <link rel="preconnect" href="https://www.googletagmanager.com" />
         <link rel="preconnect" href="https://pagead2.googlesyndication.com" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
         
-        {/* Favicon and meta */}
+        {/* Favicon and app icons */}
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#2563eb" />
+        
+        {/* Enhanced SEO meta tags */}
+        <meta name="robots" content="index, follow, max-image-preview:large" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="author" content="StreamBackdrops" />
+        <meta name="publisher" content="StreamBackdrops" />
+        
+        {/* Structured data for better SEO */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "StreamBackdrops",
+              "description": "Free HD virtual backgrounds for video calls, Zoom meetings, and professional presentations",
+              "url": "https://streambackdrops.com",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://streambackdrops.com/search?q={search_term_string}",
+                "query-input": "required name=search_term_string"
+              }
+            })
+          }}
+        />
+        
+        {/* Performance optimizations */}
+        <link rel="preload" href="/fonts/inter.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="format-detection" content="telephone=no" />
       </Head>
       <body>
         <Main />
