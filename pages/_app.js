555cd3cd206c20267c5377b0b7d2593fd0cdb4db
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-08-13 06:59:59 -0400

    gtag install

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-// pages/_app.js - Complete file with CookieYes consent management
+// pages/_app.js - Complete file with CookieYes consent management AND Google Analytics
 import '../styles/globals.css';
 import Head from 'next/head';
 import Script from 'next/script';
@@ -12,7 +12,21 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      {/* CookieYes Consent Management Script - moved outside Head */}
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QMD6NEPFWR"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QMD6NEPFWR');
+        `}
+      </Script>
+      
+      {/* CookieYes Consent Management Script */}
       <Script 
         id="cookieyes"
         src="https://cdn-cookieyes.com/client_data/8eb30da506758a4631261b2a/script.js"
