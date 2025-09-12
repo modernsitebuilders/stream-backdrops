10cba98e1707ef0f9c5efdef7e138c8e31b87703
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-09-12 18:59:19 -0400

    changed _document

--- pages/_document.js
+++ pages/_document.js
@@ -18,18 +18,13 @@ export default function Document() {
           `
         }} />
         
-        {/* CRITICAL: Add viewport meta tag */}
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        
         {/* Preconnect to external domains for faster loading */}
         <link rel="preconnect" href="https://www.googletagmanager.com" />
         <link rel="preconnect" href="https://pagead2.googlesyndication.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
-        <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
         
-        {/* FIXED: Google Tag Manager with proper syntax */}
+        {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -38,19 +33,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.defer=true;j.sr
 })(window,document,'script','dataLayer','GTM-TS54MCGR');`
         }} />
         
-        {/* Favicon and app icons */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="theme-color" content="#2563eb" />
-        
-        {/* Enhanced SEO meta tags */}
-        <meta name="robots" content="index, follow, max-image-preview:large" />
-        <meta name="googlebot" content="index, follow" />
-        <meta name="author" content="StreamBackdrops" />
-        <meta name="publisher" content="StreamBackdrops" />
-        
-        {/* SIMPLIFIED: Structured data for better SEO */}
+        {/* Basic structured data (site-level only) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -58,7 +41,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.defer=true;j.sr
               "@context": "https://schema.org",
               "@type": "WebSite",
               "name": "StreamBackdrops",
-              "description": "Free HD virtual backgrounds for video calls",
               "url": "https://streambackdrops.com"
             })
           }}
@@ -71,7 +53,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.defer=true;j.sr
         <meta name="color-scheme" content="light" />
       </Head>
       <body>
-        {/* FIXED: Google Tag Manager noscript with proper syntax */}
         <noscript>
           <iframe 
             src="https://www.googletagmanager.com/ns.html?id=GTM-TS54MCGR"
