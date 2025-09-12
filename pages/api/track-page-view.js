faba0d064be219803d9883a9c173e08348adb482
David Miles <89851396+davidmilesphilly@users.noreply.github.com>
2025-09-12 12:33:23 -0400

    adjust time on sheets files

--- pages/api/track-page-view.js
+++ pages/api/track-page-view.js
@@ -24,7 +24,15 @@ export default async function handler(req, res) {
     const sheets = google.sheets({ version: 'v4', auth });
 
     const pageViewData = [
-      new Date().toISOString(),
+      new Date().toLocaleString('en-US', {
+  timeZone: 'America/New_York',
+  year: 'numeric',
+  month: '2-digit', 
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit'
+}),
       'page_view',
       page,
       category,
